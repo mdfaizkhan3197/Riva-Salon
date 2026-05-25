47c54ef814a2c0db382aa3cd34de4e3c7e31b357
Faiz Khan <md.faizkhan3197@gmail.com>
2026-05-25 15:08:24 +0530

    Fix gallery model

--- gallery/models.py
+++ gallery/models.py
@@ -1,17 +1,32 @@
 from django.db import models
 
+
 class Gallery(models.Model):
+
     MEDIA_TYPE = (
-        ('image', 'Image'),
-        ('video', 'Video'),
+        ("image", "Image"),
+        ("video", "Video"),
     )
 
-    title = models.CharField(max_length=100)
-    media_type = models.CharField(max_length=10, choices=MEDIA_TYPE)
+    title = models.CharField(
+        max_length=100
+    )
 
-    file = models.FileField(upload_to='gallery/')
+    media_type = models.CharField(
+        max_length=10,
+        choices=MEDIA_TYPE,
+        default="image"
+    )
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(
+        upload_to="gallery/",
+        blank=False,
+        null=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
 
     def __str__(self):
         return self.title
