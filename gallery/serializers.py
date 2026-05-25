9238f8e5df231b74f6dbadd80903aae1834b8cd9
Faiz Khan <md.faizkhan3197@gmail.com>
2026-05-25 15:00:26 +0530

    Fix gallery image URLs

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Gallery
 
+
 class GallerySerializer(serializers.ModelSerializer):
+    file = serializers.SerializerMethodField()
+
     class Meta:
         model = Gallery
-        fields = '__all__'
+        fields = "__all__"
+
+    def get_file(self, obj):
+        try:
+            return obj.file.url
+        except:
+            return ""
